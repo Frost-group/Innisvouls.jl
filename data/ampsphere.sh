49fc72e10532a6e5446deac252e2e0270236856b
Jarvist Moore Frost <jarvist@gmail.com>
2025-10-18 23:15:08 +0100

    Histogram of sizes; Awk; mode 39 AAs.

--- data/ampsphere.sh
+++ data/ampsphere.sh
@@ -1,4 +1,7 @@
 wget https://ampsphere-api.big-data-biology.org/v1/downloads/AMP.tsv
 
-cat AMP.tsv | awk '{print $2}' > ampsphere.dat
+cat AMP.tsv | awk '{print $2}' | grep -v sequence  > ampsphere.dat
+
+#histogram of length
+awk ' {print(length($0))} ' ampsphere.dat | sort -n | uniq -c
 
